f7d39fc3d23cb54f2cbe0c3425330467d87460ee
pengo <mas.us.va+standas@gmail.com>
2019-01-03 21:38:45 -0500

    Added Pets and Contact info to owners on startup. Closes #33.

--- pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import sfgpetclinic.model.Owner;
+import sfgpetclinic.model.Pet;
 import sfgpetclinic.model.PetType;
 import sfgpetclinic.model.Vet;
 import sfgpetclinic.services.OwnerService;
 import sfgpetclinic.services.PetTypeService;
 import sfgpetclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,12 +40,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickeral");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickeral");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
@@ -50,7 +73,7 @@ public class DataLoader implements CommandLineRunner {
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
-        vet1.setLastName("Axes");
+        vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
